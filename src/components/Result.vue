30ddeab473dd840ecd9dc105a332122d4600d5f3
James <jamesit@d5r.ru>
2024-08-17 17:05:11 +0300

    Initial commit

---
+++ src/components/Result.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="result">
+    <div class="title">{{ results[resultIndex].title }}</div>
+    <div class="desc">
+      {{ results[resultIndex].desc }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['results', 'correctAnswers'],
+  computed: {
+    resultIndex() {
+      let index = 0;
+
+      this.results.forEach((e, i) => {
+        if (e.min <= this.correctAnswers && e.max >= this.correctAnswers) index = i;
+      })
+
+      return index;
+    }
+  }
+}
+</script>
